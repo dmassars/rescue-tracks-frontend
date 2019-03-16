aaff7a0705bc5184b3729e66e8315f4879058c90
Daniel Massarsky <alj@qlik.com>
2019-03-16 17:34:28 -0400

    started implementing new animal list

--- src/app/pages/event/select-animals/select-animals.page.ts
+++ src/app/pages/event/select-animals/select-animals.page.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnDestroy } from "@angular/core";
+import { Component, Input, OnInit, OnDestroy,  TemplateRef, ViewChild  } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 
 import { Observable, Subscription } from "rxjs";
@@ -18,6 +18,7 @@ import {
     styleUrls: ['select-animals.scss'],
 })
 export class SelectAnimalsPage implements OnInit, OnDestroy {
+
     constructor(private route: ActivatedRoute, private router: Router, private apiService: APIService) { }
 
     private paramsSub: Subscription;
@@ -26,21 +27,61 @@ export class SelectAnimalsPage implements OnInit, OnDestroy {
     public animals: Observable<Animal[]>;
     public selectedAnimals: {[key: number]: boolean};
 
+    public animalsColumns: any;
+    public animalsRows: any;
+    public animalsSelected: any;
+
+    @ViewChild('animalThumbnail') animalThumbnail: TemplateRef<any>;
+
+    onSelect({ selected }) {
+        console.log('Select Event', selected, this.animalsSelected);
+      }
+
     ngOnInit(): void {
         this.selectedAnimals = {};
         this.paramsSub = this.route.params.subscribe(params => {
             this.eventId = +params["id"];
             this.animals = this.apiService.getAnimalsForEvent(this.eventId, true);
 
-            this.animals.subscribe((animals) =>
+            this.animalsColumns = [
+                {cellTemplate: this.animalThumbnail, name: '', prop: 'CoverPhoto'},
+                {prop: 'Name'},
+                {prop: 'Breed'},
+                {prop: 'Sex'},
+                {prop: 'CurrentWeightPounds', name: 'Weight'},
+                {prop: 'Age'},
+                {prop: 'Color'},
+                {prop: 'Fee'}
+
+            ]
+
+            this.animals.subscribe((animals) => {
+                this.animalsSelected = [
+                    ..._.filter(animals,'selected')
+                ]
+                this.animalsRows = [...
+                    _.forEach(animals,(e)=>{ 
+                        let ageInMonths = _.toInteger(e.Age)
+                        let years = Math.floor(ageInMonths/12)
+                        let months = ageInMonths % 12
+
+                        e.Age = `${years} Y, ${months} M`
+                        e.Fee = e.AdoptionFeeGroup.Name
+
+                        return e
+                    })
+                ]
+                console.log(animals)
+
                 _.chain(animals)
                     .filter("selected")
                     .map("externalId")
                     .each((id: number) => {
                         this.selectedAnimals[id] = true;
+
                     })
                     .value()
-            )
+                })
         });
     }
 
